6db42bf3860e2409787d537ce1af176b0fa24e0c
Tim Sutton <tim@kartoza.com>
2025-05-09 18:09:00 +0100

    loki fixes

--- loki-promtail.nix
+++ loki-promtail.nix
@@ -8,60 +8,116 @@
     enable = true;
     configuration = {
       auth_enabled = false;
-      server.http_listen_port = 3100;
-      ingester.lifecycler.ring.kvstore.store = "inmemory";
-      ingester.lifecycler.ring.replication_factor = 1;
-      schema_config.configs = [
-        {
-          from = "2020-10-24";
-          store = "boltdb-shipper";
-          object_store = "filesystem";
-          schema = "v11";
-          index = {
-            prefix = "index_";
-            period = "168h";
+
+      server = {
+        http_listen_port = 3100;
+      };
+
+      common = {
+        path_prefix = "/var/lib/loki";
+        storage = {
+          filesystem = {
+            chunks_directory = "/var/lib/loki/chunks";
+            rules_directory = "/var/lib/loki/rules";
           };
-        }
-      ];
-      storage_config = {
-        boltdb_shipper.active_index_directory = "/var/lib/loki/index";
-        boltdb_shipper.cache_location = "/var/lib/loki/cache";
-        filesystem.directory = "/var/lib/loki/chunks";
+        };
+        replication_factor = 1;
+        ring = {
+          kvstore = {
+            store = "inmemory";
+          };
+        };
+      };
+
+      schema_config = {
+        configs = [
+          {
+            from = "2023-01-01";
+            store = "boltdb-shipper";
+            object_store = "filesystem";
+            schema = "v12"; # Updated schema version
+            index = {
+              prefix = "index_";
+              period = "24h"; # Set to 24h period as recommended
+            };
+          }
+        ];
+      };
+
+      limits_config = {
+        allow_structured_metadata = false; # Disable structured metadata until we upgrade to v13
+      };
+
+      ruler = {
+        alertmanager_url = "http://localhost:9093";
+        storage = {
+          type = "local";
+          local = {
+            directory = "/var/lib/loki/rules";
+          };
+        };
+        rule_path = "/var/lib/loki/rules";
+        ring = {
+          kvstore = {
+            store = "inmemory";
+          };
+        };
+      };
+
+      compactor = {
+        working_directory = "/var/lib/loki/compactor"; # Set working directory
+        retention_enabled = true;
+        compaction_interval = "5m";
+      };
+
+      analytics = {
+        reporting_enabled = false;
       };
     };
   };
 
+  # Promtail configuration
   services.promtail = {
     enable = true;
     configuration = {
-      server.http_listen_port = 9080;
-      clients = [{url = "http://localhost:3100/loki/api/v1/push";}];
+      server = {
+        http_listen_port = 9080;
+        grpc_listen_port = 0;
+      };
+      positions = {
+        filename = "/var/lib/promtail/positions.yaml";
+      };
+      clients = [
+        {
+          url = "http://localhost:3100/loki/api/v1/push";
+        }
+      ];
       scrape_configs = [
         {
-          job_name = "system";
-          static_configs = [
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "${config.networking.hostName}";
+            };
+          };
+          relabel_configs = [
             {
-              targets = ["localhost"];
-              labels = {
-                job = "system";
-                __path__ = "/var/log/**/*.log";
-              };
-            }
-          ];
-        }
-        {
-          job_name = "docker";
-          static_configs = [
-            {
-              targets = ["localhost"];
-              labels = {
-                job = "docker";
-                __path__ = "/var/lib/docker/containers/*/*.log";
-              };
+              source_labels = ["__journal__systemd_unit"];
+              target_label = "unit";
             }
           ];
         }
       ];
     };
   };
+
+  # Create necessary directories
+  systemd.tmpfiles.rules = [
+    "d /var/lib/loki 0750 loki loki -"
+    "d /var/lib/loki/chunks 0750 loki loki -"
+    "d /var/lib/loki/rules 0750 loki loki -"
+    "d /var/lib/loki/compactor 0750 loki loki -"
+  ];
 }
