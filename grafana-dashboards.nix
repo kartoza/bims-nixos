629e87202aef841780c7531f61c5e3fcc5ea270c
Tim Sutton <tim@kartoza.com>
2025-05-09 17:59:46 +0100

    Fixes for dashboards

--- grafana-dashboards.nix
+++ grafana-dashboards.nix
@@ -2,23 +2,20 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
+  # Make sure dashboard JSON files exist in ./dashboards/ directory
   services.grafana = {
-    settings = {
-      server = {
-        # Any server settings here
-      };
-    };
-
     provision = {
-      # New structure for provisioning dashboards
-      dashboards = {
-        settings = [
+      # Alternative approach
+      dashboards.path = "${pkgs.writeTextDir "dashboards.yaml" (builtins.toJSON {
+        apiVersion = 1;
+        providers = [
           {
             name = "default";
             orgId = 1;
-            folder = "Dashboards";
+            folder = "";
             type = "file";
             disableDeletion = false;
             updateIntervalSeconds = 10;
@@ -28,25 +25,21 @@
             };
           }
         ];
-      };
+      })}";
     };
   };
 
-  # Then ensure your dashboard files are placed in the correct location
+  # Place dashboard json files in /etc/grafana/dashboards/
   environment.etc = {
     "grafana/dashboards/system-dashboard.json" = {
       source = ./dashboards/system-dashboard.json;
       mode = "0644";
-      user = "grafana";
-      group = "grafana";
     };
-    # Add other dashboards as needed
   };
 
-  # Create the directory for storing dashboards
+  # Create symlinks from Grafana's dashboard directory to our files
   systemd.tmpfiles.rules = [
     "d /var/lib/grafana/dashboards 0755 grafana grafana -"
     "L+ /var/lib/grafana/dashboards/system-dashboard.json - - - - /etc/grafana/dashboards/system-dashboard.json"
-    # Add symlinks for other dashboards
   ];
 }
